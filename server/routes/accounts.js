de51eefe004210257e791208ccee9810fb6a6cd2
Kent Korgan <korgankd@dukes.jmu.edu>
2017-01-03 08:06:00 -0500

    added photos, video still not working

--- server/routes/accounts.js
+++ server/routes/accounts.js
@@ -1,6 +1,6 @@
 var accounts = [
-	{id:0, user:"korgankd", password:"password", name:"Kent Korgan", description: "This is the description of the Kent Korgan account. It should give some information about this user.", availability:"Kent's availability", media:"Kent's media"},
-	{id:1, user:"kentcl", password:"password", name:"Clark Kent", description: "This is the description of the Clark Kent account. It should give some information about this user.", availability:"Clark's availability", media:"Clark's media"}
+	{id:0, user:"korgankd", password:"password", name:"Kent Korgan", description: "This is the description of the Kent Korgan account. It should give some information about this user.", availability:"Kent's availability", media:"Kent's media", location:"11257 Ramrod Road, Woodbridge VA", image:"#/Capture.jpg", video:"https://vimeo.com/106695936"},
+	{id:1, user:"kentcl", password:"password", name:"Clark Kent", description: "This is the description of the Clark Kent account. It should give some information about this user.", availability:"Clark's availability", media:"Clark's media", location:"North Pole Fortress of Solitude", image:"http://i.huffpost.com/gen/964776/images/o-CATS-KILL-BILLIONS-facebook.jpg", video:"blank"}
 ];
 
 exports.findAll = function(req, res, next) {
